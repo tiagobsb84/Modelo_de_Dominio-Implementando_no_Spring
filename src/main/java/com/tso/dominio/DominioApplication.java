dcfcd9ab4a01591849736b7c14497c4463018466
Tiago Silva <tiagobsb84@gmail.com>
2022-02-07 14:42:14 -0300

    Criando projeto

---
+++ src/main/java/com/tso/dominio/DominioApplication.java
@@ -0,0 +1,12 @@
+package com.tso.dominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominioApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tso/dominio/DominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tso.dominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
